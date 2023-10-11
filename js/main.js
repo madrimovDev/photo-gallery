5eb244f5030f0fd21de51eb73c6e3d5149839978
madrimovDev <madrimov5014@gmail.com>
2023-10-11 14:44:55 +0500

    add

--- js/main.js
+++ js/main.js
@@ -1,9 +1,50 @@
-let API_URL = "https://api.unsplash.com/photos/?client_id=Mmq9Xan9yMp_3eRZZISIQhtIBZoJLHUyXp01TmCk418"
+let API_URL = "https://api.unsplash.com/photos?query=mountains"
+
+let columns = document.querySelectorAll('.column')
 
 // malumot so'rash - GET
 
-fetch(API_URL).then(response => {
+// [{}, {}, {}, {}]
+
+fetch(API_URL, {
+  headers: {
+    Authorization: "Client-ID Mmq9Xan9yMp_3eRZZISIQhtIBZoJLHUyXp01TmCk418"
+  },
+}).then(response => {
   return response.json()
 }).then(result => {
-  console.log(result)
+  const res1 = result.slice(0, 3)
+  const res2 = result.slice(3, 6 )
+  const res3 = result.slice(6, 10 )
+
+  res1.forEach((item, index) => {
+    let img = document.createElement('img')
+    img.src = item.urls.regular
+    columns.forEach((column, index) => {
+      if(index === 0){
+        column.append(img)
+      }
+    })
+  })
+
+  res2.forEach((item, index) => {
+    let img = document.createElement('img')
+    img.src = item.urls.regular
+    columns.forEach((column, index) => {
+      if(index === 1){
+        column.append(img)
+      }
+    })
+  })
+  
+  res3.forEach((item, index) => {
+    let img = document.createElement('img')
+    img.src = item.urls.regular
+    columns.forEach((column, index) => {
+      if(index === 2){
+        column.append(img)
+      }
+    })
+  })
 })
+
